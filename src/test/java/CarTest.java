b47b3c7007db86f22fbf0f2438be39f2d618bc2a
kpilat98 <kacper.pilat@wp.pl>
2022-03-08 17:38:00 +0100

    making refactor

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -6,7 +6,11 @@ public class CarTest {
     BrakesTest testBrakes = new BrakesTest();
     ChassisTest testChassis = new ChassisTest();
     String model;
-
+    public void viewResult(){
+        System.out.println(model+ " " + testEngine.testResult);
+        System.out.println(model+ " " + testBrakes.testResult);
+        System.out.println(model+ " " + testChassis.testResult);
+    }
     @Test
     public  void testFiat500(){
         model = "fiat500";
@@ -14,9 +18,7 @@ public class CarTest {
         testBrakes.brakesTest(car.fiat500.brakes,car.fiat500.type);
         testEngine.engineTest(car.fiat500.fuel,car.fiat500.emission,car.fiat500.type);
         testChassis.chassisTest(car.fiat500.mass,car.fiat500.axleNumber,car.fiat500.type);
-        System.out.println(model+ " " + testEngine.testResult);
-        System.out.println(model+ " " + testBrakes.testResult);
-        System.out.println(model+ " " + testChassis.testResult);
+        viewResult();
     }
     @Test
     public void testOpelVivaro(){
@@ -25,9 +27,7 @@ public class CarTest {
         testBrakes.brakesTest(car.opelVivaro.brakes,car.opelVivaro.type);
         testEngine.engineTest(car.opelVivaro.fuel,car.opelVivaro.emission,car.opelVivaro.type);
         testChassis.chassisTest(car.opelVivaro.mass,car.opelVivaro.axleNumber,car.opelVivaro.type);
-        System.out.println(model+ " " + testEngine.testResult);
-        System.out.println(model+ " " + testBrakes.testResult);
-        System.out.println(model+ " " + testChassis.testResult);
+        viewResult();
     }
 
     @Test
@@ -37,9 +37,7 @@ public class CarTest {
         testBrakes.brakesTest(car.scaniaR450.brakes,car.scaniaR450.type);
         testEngine.engineTest(car.scaniaR450.fuel,car.scaniaR450.emission,car.scaniaR450.type);
         testChassis.chassisTest(car.scaniaR450.mass,car.scaniaR450.axleNumber,car.scaniaR450.type);
-        System.out.println(model+ " " + testEngine.testResult);
-        System.out.println(model+ " " + testBrakes.testResult);
-        System.out.println(model+ " " + testChassis.testResult);
+        viewResult();
 
     }
 }
